470529a4ace93da5df7dc4d646a3963906ba1a3a
Ville Ahti <vianah@utu.fi>
2012-03-08 15:31:54 +0200

    jou

--- gen/com/android/kalenteri/R.java
+++ gen/com/android/kalenteri/R.java
@@ -14,21 +14,25 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int button1=0x7f050005;
         public static final int editText1=0x7f050002;
         public static final int editText2=0x7f050004;
         public static final int textView1=0x7f050000;
         public static final int textView2=0x7f050001;
         public static final int textView3=0x7f050003;
+        public static final int textView4=0x7f050006;
     }
     public static final class layout {
         public static final int login=0x7f030000;
         public static final int main=0x7f030001;
     }
     public static final class string {
+        public static final int Login_button=0x7f040005;
         public static final int Login_pass=0x7f040004;
         public static final int Login_title=0x7f040002;
         public static final int Login_user=0x7f040003;
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
+        public static final int kirjautuminen=0x7f040006;
     }
 }
--- src/com/android/kalenteri/AndroidKalenteriActivity.java
+++ src/com/android/kalenteri/AndroidKalenteriActivity.java
@@ -2,12 +2,39 @@ package com.android.kalenteri;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class AndroidKalenteriActivity extends Activity {
+	
+	private static String[] kayttaja = {"user","password"};
+	private EditText kayttajanimikentta;
+	private EditText salasanakentta;
+	private Button nappula;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        
+        kayttajanimikentta = (EditText)findViewById(R.id.editText1);
+        salasanakentta = (EditText) findViewById(R.id.editText2);
+        nappula = (Button) findViewById(R.id.button1);
+        
+        nappula.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				if(kayttajanimikentta.getText().toString() == kayttaja[0] && 
+						salasanakentta.getText().toString() == kayttaja[1]) {
+					TextView teksti = (TextView) findViewById(R.id.textView4);
+					teksti.setText(R.string.kirjautuminen);
+				}
+			}
+		});
+        
     }
 }
