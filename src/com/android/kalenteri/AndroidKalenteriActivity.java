379e08b7e7f6318df8bb5ed2445241ea9f75404c
Ville Ahti <vianah@utu.fi>
2012-03-14 14:34:41 +0200

    jotain

--- src/com/android/kalenteri/AndroidKalenteriActivity.java
+++ src/com/android/kalenteri/AndroidKalenteriActivity.java
@@ -2,43 +2,15 @@ package com.android.kalenteri;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import com.android.kalenteri.muut.*;
 
 public class AndroidKalenteriActivity extends Activity {
-	
-	private static String[] kayttaja = {"user","password"};
-	private EditText kayttajanimikentta;
-	private EditText salasanakentta;
-	private Button nappula;
-	
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
-        
-        kayttajanimikentta = (EditText)findViewById(R.id.editText1);
-        salasanakentta = (EditText) findViewById(R.id.editText2);
-        nappula = (Button) findViewById(R.id.button1);
-        
-        nappula.setOnClickListener(new View.OnClickListener() {
-			
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				if(kayttajanimikentta.getText().toString().equals(kayttaja[0]) && 
-						salasanakentta.getText().toString().equals(kayttaja[1])) {
-					TextView teksti = (TextView) findViewById(R.id.textView4);
-					teksti.setText(R.string.kirjautuminen);
-				}
-				else {
-					TextView teksti = (TextView) findViewById(R.id.textView4);
-					teksti.setText("ei onnistu");
-				}
-			}
-		});
-        
-    }
+
+	protected User user;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
 }
