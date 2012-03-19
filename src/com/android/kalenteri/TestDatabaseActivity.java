31e20f3f3a8a837722d91bbef95054dca5632a87
Ville Ahti <vianah@utu.fi>
2012-03-19 20:03:24 +0200

    database valmis?

--- src/com/android/kalenteri/TestDatabaseActivity.java
+++ src/com/android/kalenteri/TestDatabaseActivity.java
@@ -4,14 +4,22 @@ import com.android.kalenteri.database.*;
 
 import java.util.List;
 
+import org.w3c.dom.Text;
+
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class TestDatabaseActivity extends ListActivity {
 	
 	private UserCourseDatabase datasource;
+	private Button addCourse;
+	private Button addUserToCourse;
+	private TextView failureInfo;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -28,6 +36,26 @@ public class TestDatabaseActivity extends ListActivity {
 		ArrayAdapter<User> adapter = new ArrayAdapter<User>(this,
 				android.R.layout.simple_list_item_1, values);
 		setListAdapter(adapter);
+		
+	    failureInfo = (TextView) findViewById(R.id.course_failure);
+		
+		addCourse = (Button) findViewById(R.id.test_addCourse);
+		addCourse.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				 failureInfo.setText(new Boolean(datasource.createCourse("Matikka", 3)).toString());
+				
+			}
+		});
+		addUserToCourse = (Button) findViewById(R.id.test_addUserToCourse);
+		addUserToCourse.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				failureInfo.setText(new Boolean(datasource.userToCourse(1, 1)).toString());
+			}
+		});
 	}
 
 	// Will be called via the onClick attribute
--- src/com/android/kalenteri/database/UserCourseDatabase.java
+++ src/com/android/kalenteri/database/UserCourseDatabase.java
@@ -14,8 +14,10 @@ import android.database.sqlite.SQLiteDatabase;
 public class UserCourseDatabase {
 	private SQLiteDatabase database;
 	private DbSQLiteHelper dbHelper;
-	private String[] allColumns = {DbSQLiteHelper.COLUMN_ID, DbSQLiteHelper.COLUMN_USERNAME, 
-			DbSQLiteHelper.COLUMN_PASSWORD, DbSQLiteHelper.COLUMN_USERTYPE};
+	private String[] allUserColumns = {DbSQLiteHelper.USER_COLUMN_ID, DbSQLiteHelper.USER_COLUMN_USERNAME, 
+			DbSQLiteHelper.USER_COLUMN_PASSWORD, DbSQLiteHelper.USER_COLUMN_USERTYPE};
+	private String[] allCourseColumns = {DbSQLiteHelper.COURSES_COLUMN_ID, DbSQLiteHelper.COURSES_COLUMN_NAME, 
+			DbSQLiteHelper.COURSES_COLUMN_POINTS};
 
 	public UserCourseDatabase(Context context) {
 		// TODO Auto-generated constructor stub
@@ -32,18 +34,38 @@ public class UserCourseDatabase {
 
 	public User createUser(String username, String password, int userType) {
 		ContentValues values = new ContentValues();
-		values.put(DbSQLiteHelper.COLUMN_USERNAME, username);
-		values.put(DbSQLiteHelper.COLUMN_PASSWORD, password);
-		values.put(DbSQLiteHelper.COLUMN_USERTYPE, userType);
+		values.put(DbSQLiteHelper.USER_COLUMN_USERNAME, username);
+		values.put(DbSQLiteHelper.USER_COLUMN_PASSWORD, password);
+		values.put(DbSQLiteHelper.USER_COLUMN_USERTYPE, userType);
 		long insertId = database.insert(DbSQLiteHelper.TABLE_USERS, null,
 				values);
 		// To show how to query
 		Cursor cursor = database.query(DbSQLiteHelper.TABLE_USERS,
-				allColumns, DbSQLiteHelper.COLUMN_ID + " = " + insertId, null,
+				allUserColumns, DbSQLiteHelper.USER_COLUMN_ID + " = " + insertId, null,
 				null, null, null);
 		cursor.moveToFirst();
 		return cursorToUser(cursor);
 	}
+	
+	public boolean createCourse(String cName, int cPoints) {
+		ContentValues values = new ContentValues();
+		values.put(DbSQLiteHelper.COURSES_COLUMN_NAME, cName);
+		values.put(DbSQLiteHelper.COURSES_COLUMN_POINTS, cPoints);
+		long insertId = database.insert(DbSQLiteHelper.TABLE_COURSES, null,
+				values);
+		return (insertId != -1);
+	}
+	
+	public boolean userToCourse(int userId, int courseId ) {
+		ContentValues values = new ContentValues();
+		values.put(DbSQLiteHelper.TAKES_COLUMN_USER_ID, userId);
+		values.put(DbSQLiteHelper.TAKES_COLUMN_COURSE_ID, courseId);
+		values.put(DbSQLiteHelper.TAKES_COLUMN_FINISHED, 0);
+		long insertId = database.insert(DbSQLiteHelper.TABLE_TAKES, null,
+				values);
+		return (insertId != -1);
+	}
+	
 	
 	private User cursorToUser(Cursor cursor) {
 		User user = new User((int)cursor.getLong(0), 
@@ -54,7 +76,7 @@ public class UserCourseDatabase {
 	public List<User> getAllUsers() {
 		List<User> users = new ArrayList<User>();
 		Cursor cursor = database.query(DbSQLiteHelper.TABLE_USERS,
-				allColumns, null, null, null, null, null);
+				allUserColumns, null, null, null, null, null);
 		cursor.moveToFirst();
 		while (!cursor.isAfterLast()) {
 			User user = cursorToUser(cursor);
